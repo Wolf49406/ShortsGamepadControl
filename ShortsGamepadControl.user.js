c97670ee77c305aebd0867290eb716039736c5f6
Alexander Wolf <fake.wolf@2ch.hk>
2026-07-22 15:13:53 +1000

    Massive refactor

--- ShortsGamepadControl.user.js
+++ ShortsGamepadControl.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         YouTube Shorts Gamepad Control
-// @version      1.2.0
+// @version      2.0.0
 // @description  Take a Full Control on Youtube Shorts with Gamepad
 // @author       https://github.com/Wolf49406
 // @match        http*://www.youtube.com/*
@@ -11,19 +11,246 @@
 // @grant        none
 // ==/UserScript==
 
-// Global objects
-let g_currentContainer;
-let g_currentVideo;
-let g_observer;
-let g_gamepadIndex;
-let g_pressedButtonIndex;
+const App = {
+  config: {
+    seek_time: 3,
+    vibrate: true,
+    debug: true,
+  },
 
-// Config
-const g_seekTime = 3;
-const g_vibrate = true;
-const g_debug = false;
+  state: {
+    observer: null,
+    gamepad_index: -1,
+    pressed_button_index: -1,
+    seen_reels: null,
+  },
 
-// Buttons enum
+  DOM: {
+    like_button:
+      "#experiment-overlay > ytd-reel-player-overlay-renderer > yt-reel-player-overlay-view-model > div.ytReelPlayerOverlayViewModelActionsContainer > reel-action-bar-view-model > like-button-view-model > toggle-button-view-model > button-view-model > label > button > yt-touch-feedback-shape > div",
+    shorts_inner_container: "shorts-inner-container",
+    video: "#shorts-player > div.html5-video-container > video",
+    reel_video_in_sequence_new: "reel-video-in-sequence-new",
+    reel_video_renderer: "#reel-video-renderer",
+  },
+
+  Logger: {
+    debug(message) {
+      if (App.config.debug) console.log(`[SGC] ${message}`);
+    },
+    info(message) {
+      console.info(`[SGC] ${message}`);
+    },
+    error(message) {
+      console.error(`[SGC] ${message}`);
+    },
+  },
+
+  Actions: {
+    ValidateURL() {
+      // Tampermonkey's @match not gonna work with SPA
+      return location.href.startsWith(`https://www.youtube.com/shorts/`);
+    },
+
+    GetReels() {
+      const reels = document.getElementsByClassName(
+        App.DOM.reel_video_in_sequence_new,
+      );
+      if (!reels || reels.length === 0) {
+        App.Logger.error("GetReels: !reels");
+        return null;
+      }
+
+      App.state.seen_reels = new WeakSet();
+
+      for (let i = 0; i < reels.length; i++) {
+        const reel = reels[i];
+        if (
+          !App.state.seen_reels.has(reel) &&
+          reel.querySelector(App.DOM.reel_video_renderer)
+        ) {
+          App.state.seen_reels.add(reel);
+
+          return reel;
+        }
+      }
+
+      return null;
+    },
+
+    GetCurrentVideo() {
+      const current_container = App.Actions.GetReels();
+      if (!current_container) {
+        App.Logger.error("GetCurrentVideo: !current_container");
+        return null;
+      }
+
+      const video = current_container.querySelector(App.DOM.video);
+      if (!video) {
+        App.Logger.error("GetCurrentVideo: !video");
+        return null;
+      }
+
+      return video;
+    },
+
+    GetLikeButton() {
+      const current_container = App.Actions.GetReels();
+      if (!current_container) {
+        App.Logger.error("GetLikeButton: !current_container");
+        return null;
+      }
+
+      const button = current_container.querySelector(App.DOM.like_button);
+      if (!button) {
+        App.Logger.error("GetLikeButton: !button");
+        return null;
+      }
+
+      return button;
+    },
+
+    InitApp() {
+      if (App.state.observer) App.state.observer.disconnect();
+
+      App.state.observer = new MutationObserver(() => {
+        App.Logger.debug("MutationObserver: DOM Changed");
+      });
+
+      const WaitForShortsContainer = setInterval(() => {
+        const container = document.getElementById(
+          App.DOM.shorts_inner_container,
+        );
+        if (container) {
+          clearInterval(WaitForShortsContainer);
+          App.state.observer.observe(container, {
+            childList: true,
+            subtree: true,
+          });
+        }
+      }, 200);
+    },
+
+    Vibrate() {
+      if (!App.config.vibrate || App.state.gamepad_index === -1) return;
+
+      const gamepad = navigator.getGamepads()[App.state.gamepad_index];
+      if (!gamepad) {
+        App.Logger.error("Vibrate: Gamepad not found");
+        return;
+      }
+
+      if (gamepad.vibrationActuator && gamepad.vibrationActuator.playEffect) {
+        gamepad.vibrationActuator.playEffect("dual-rumble", {
+          duration: 150, // duration in milliseconds
+          weakMagnitude: 1, // intensity (0-1) of the small ERM
+          strongMagnitude: 1, // intesity (0-1) of the bigger ERM
+        });
+      }
+    },
+
+    Player: {
+      PlayPause() {
+        const current_video = App.Actions.GetCurrentVideo();
+        if (!current_video) {
+          App.Logger.error("Player PlayPause: !current_video");
+          return;
+        }
+
+        current_video.paused ? current_video.play() : current_video.pause();
+        App.Actions.Vibrate();
+      },
+
+      SetTime(current_video, time) {
+        let current_time = current_video.currentTime; // Default HTML5 Video/Audio API — https://www.w3schools.com/tags/ref_av_dom.asp
+        current_video.currentTime = current_time + time;
+      },
+
+      Next() {
+        const current_container = App.Actions.GetReels();
+        if (!current_container) {
+          App.Logger.error("Player Next: !current_container");
+          return;
+        }
+
+        const currentId = Number(current_container.id);
+        if (Number.isNaN(currentId)) {
+          App.Logger.error(
+            `Player Next: Invalid container id -> ${current_container.id}`,
+          );
+          return;
+        }
+
+        const next = document.getElementById(currentId + 1);
+        if (!next) {
+          App.Logger.error("Player Next: !next");
+          return;
+        }
+
+        next.scrollIntoView({
+          behavior: "smooth",
+          block: "end",
+        });
+
+        App.Actions.Vibrate();
+      },
+
+      Prev() {
+        const current_container = App.Actions.GetReels();
+        if (!current_container) {
+          App.Logger.error("Player Prev: !current_container");
+          return;
+        }
+
+        const currentId = Number(current_container.id);
+        if (Number.isNaN(currentId) || currentId <= 0) {
+          App.Logger.error(
+            `Player Prev: Invalid container id -> ${current_container.id}`,
+          );
+          return;
+        }
+
+        const prev = document.getElementById(currentId - 1);
+        if (!prev) {
+          App.Logger.error("Player Prev: !prev");
+          return;
+        }
+
+        App.state.seen_reels.delete(prev);
+
+        prev.scrollIntoView({
+          behavior: "smooth",
+          block: "end",
+        });
+
+        App.Actions.Vibrate();
+      },
+
+      Like() {
+        const like_button = App.Actions.GetLikeButton();
+        if (!like_button) {
+          App.Logger.error("Player Like: !LikeButton");
+          return;
+        }
+
+        like_button.click();
+        App.Actions.Vibrate();
+      },
+
+      Seek(offset) {
+        const current_video = App.Actions.GetCurrentVideo();
+        if (!current_video) {
+          App.Logger.error("Player Seek: !current_video");
+          return;
+        }
+
+        App.Actions.Player.SetTime(current_video, offset);
+        App.Actions.Vibrate();
+      },
+    },
+  },
+};
+
 const Button_t = {
   A: 0,
   B: 1,
@@ -47,278 +274,59 @@ const Button_t = {
 (function () {
   "use strict";
 
-  ///////////////////////
-  // Helpers Functions //
-  ///////////////////////
-
-  const Logger = {
-    debug(message) {
-      if (g_debug) console.log(`[SGC] ${message}`);
-    },
-    info(message) {
-      console.info(`[SGC] ${message}`);
-    },
-    error(message) {
-      console.error(`[SGC] ${message}`);
-    },
-  };
-
-  function GetCurrentVideo(container) {
-    if (!container) {
-      Logger.error("GetCurrentVideo: !container");
-      return undefined;
-    }
-
-    const video = container.querySelector(
-      "#shorts-player > div.html5-video-container > video",
-    );
-
-    if (!video) {
-      Logger.error("GetCurrentVideo: !video");
-      return undefined;
-    }
-
-    return video;
-  }
-
-  function GetLikeButton(container) {
-    if (!container) {
-      Logger.error("GetLikeButton: !container");
-      return undefined;
-    }
-
-    const button = container.querySelector(
-      "#experiment-overlay > ytd-reel-player-overlay-renderer > yt-reel-player-overlay-view-model > div.ytReelPlayerOverlayViewModelActionsContainer > reel-action-bar-view-model > like-button-view-model > toggle-button-view-model > button-view-model > label > button > yt-touch-feedback-shape > div",
-    );
-
-    if (!button) {
-      Logger.error("GetLikeButton: !button");
-      return undefined;
-    }
-
-    return button;
-  }
-
-  function SetTime(video, time) {
-    let currentTime = video.currentTime; // Default HTML5 Video/Audio API — https://www.w3schools.com/tags/ref_av_dom.asp
-    video.currentTime = currentTime + time;
-  }
-
-  // Tampermonkey's @match not gonna work with SPA
-  function IsValidURL() {
-    return location.href.startsWith(`https://www.youtube.com/shorts/`);
-  }
-
-  function Vibrate() {
-    if (!g_vibrate) return;
-
-    const Gamepad = navigator.getGamepads()[g_gamepadIndex];
-    if (Gamepad.vibrationActuator && Gamepad.vibrationActuator.playEffect) {
-      Gamepad.vibrationActuator.playEffect("dual-rumble", {
-        duration: 150, // Duration in milliseconds
-        weakMagnitude: 1, // intensity (0-1) of the small ERM
-        strongMagnitude: 1, // intesity (0-1) of the bigger ERM
-      });
-    }
-  }
-
-  //////////////////////////////
-  // Player-Related Functions //
-  //////////////////////////////
-
-  function Player_PlayPause() {
-    if (!g_currentVideo) {
-      Logger.error("Player_PlayPause: !g_currentVideo");
-      return;
-    }
-
-    g_currentVideo.paused ? g_currentVideo.play() : g_currentVideo.pause();
-    Vibrate();
-  }
-
-  function Player_Next() {
-    if (!g_currentContainer) {
-      Logger.error("Player_Next: !g_currentContainer");
-      return;
-    }
-
-    const nextId = parseInt(g_currentContainer.id) + 1;
-    if (isNaN(nextId)) {
-      Logger.error("Player_Next: isNaN(nextId)");
-      return;
-    }
-
-    const next = document.getElementById(nextId);
-    if (!next) {
-      Logger.error("Player_Next: !next");
-      return;
-    }
-
-    g_currentContainer = next;
-    g_currentVideo = GetCurrentVideo(next);
-
-    next.scrollIntoView({
-      behavior: "smooth",
-      block: "end",
-    });
-    Vibrate();
-  }
-
-  function Player_Prev() {
-    if (!g_currentContainer) {
-      Logger.error("Player_Prev: !g_currentContainer");
-      return;
-    }
-
-    const prevId = parseInt(g_currentContainer.id) - 1;
-    if (isNaN(prevId)) {
-      Logger.error("Player_Prev: isNaN(prevId)");
-      return;
-    }
-
-    const prev = document.getElementById(prevId);
-    if (!prev) {
-      Logger.error("Player_Prev: !prev");
-      return;
-    }
-
-    seenReels.delete(prev);
-    g_currentContainer = prev;
-    g_currentVideo = GetCurrentVideo(prev);
-
-    prev.scrollIntoView({
-      behavior: "smooth",
-      block: "end",
-    });
-    Vibrate();
-  }
-
-  function Player_Like() {
-    const LikeButton = GetLikeButton(g_currentContainer);
-    if (!LikeButton) {
-      Logger.error("Player_Like: !LikeButton");
-      return;
-    }
-
-    LikeButton.click();
-    Vibrate();
-  }
-
-  function Player_SeekForward() {
-    if (!g_currentVideo) {
-      Logger.error("Player_SeekForward: !g_currentVideo");
-      return;
-    }
-
-    SetTime(g_currentVideo, +g_seekTime);
-    Vibrate();
-  }
-
-  function Player_SeekBack() {
-    if (!g_currentVideo) {
-      Logger.error("Player_SeekBack: !g_currentVideo");
-      return;
-    }
-
-    SetTime(g_currentVideo, -g_seekTime);
-    Vibrate();
-  }
-
-  /////////////////////////////
-  // Button-Binded Functions //
-  /////////////////////////////
+  /////////////////////
+  // Button Bindings //
+  /////////////////////
 
   const buttonBindings = [];
-  buttonBindings[Button_t.Y] = Player_Like;
 
-  buttonBindings[Button_t.X] = Player_Prev;
-  buttonBindings[Button_t.A] = Player_Next;
+  buttonBindings[Button_t.Y] = App.Actions.Player.Like;
 
-  buttonBindings[Button_t.ARROW_UP] = Player_Prev;
-  buttonBindings[Button_t.ARROW_DOWN] = Player_Next;
+  buttonBindings[Button_t.LT] = App.Actions.Player.PlayPause;
+  buttonBindings[Button_t.RT] = App.Actions.Player.PlayPause;
+  
+  buttonBindings[Button_t.X] = App.Actions.Player.Prev;
+  buttonBindings[Button_t.A] = App.Actions.Player.Next;
 
-  buttonBindings[Button_t.ARROW_LEFT] = Player_SeekBack;
-  buttonBindings[Button_t.ARROW_RIGHT] = Player_SeekForward;
+  buttonBindings[Button_t.ARROW_UP] = App.Actions.Player.Prev;
+  buttonBindings[Button_t.ARROW_DOWN] = App.Actions.Player.Next;
 
-  buttonBindings[Button_t.LB] = Player_SeekBack;
-  buttonBindings[Button_t.RB] = Player_SeekForward;
+  buttonBindings[Button_t.ARROW_LEFT] = () => App.Actions.Player.Seek(-App.config.seek_time);
+  buttonBindings[Button_t.ARROW_RIGHT] = () => App.Actions.Player.Seek(+App.config.seek_time);
 
-  buttonBindings[Button_t.LT] = Player_PlayPause;
-  buttonBindings[Button_t.RT] = Player_PlayPause;
+  buttonBindings[Button_t.LB] = () => App.Actions.Player.Seek(-App.config.seek_time);
+  buttonBindings[Button_t.RB] = () => App.Actions.Player.Seek(+App.config.seek_time);
 
   function HandleButton(buttonIndex) {
     const Binding = buttonBindings[buttonIndex];
     if (Binding) Binding();
   }
 
-  ///////////////////////////////
-  // Main Shorts Update Worker //
-  ///////////////////////////////
-
-  const seenReels = new WeakSet();
-
-  function InitObserver() {
-    if (g_observer) g_observer.disconnect();
-
-    g_observer = new MutationObserver(() => {
-      if (!IsValidURL()) return;
-
-      const reels = document.getElementsByClassName(
-        "reel-video-in-sequence-new",
-      );
-
-      for (let i = 0; i < reels.length; i++) {
-        const reel = reels[i];
-        if (
-          !seenReels.has(reel) &&
-          reel.querySelector("#reel-video-renderer")
-        ) {
-          const CurrentVideo = GetCurrentVideo(reel);
-
-          if (CurrentVideo) {
-            seenReels.add(reel);
-
-            g_currentContainer = reel;
-            g_currentVideo = CurrentVideo;
-
-            Logger.info(`Observer: New Container -> ${g_currentContainer.id}`);
-            break;
-          }
-        }
-      }
-    });
-
-    const WaitForShortsContainer = setInterval(() => {
-      const container = document.getElementById("shorts-inner-container");
-      if (container) {
-        clearInterval(WaitForShortsContainer);
-        g_observer.observe(container, { childList: true, subtree: true });
-      }
-    }, 200);
-  }
-
-  /////////////////////////
-  // Main Buttons Worker //
-  /////////////////////////
+  ////////////////////
+  // Buttons Worker //
+  ////////////////////
 
   setInterval(() => {
-    if (g_gamepadIndex == undefined || !IsValidURL()) return;
+    if (App.state.gamepad_index === -1 || !App.Actions.ValidateURL()) return;
 
-    const gamepad = navigator.getGamepads()[g_gamepadIndex];
-    gamepad.buttons
-      .map((e) => e.pressed)
-      .forEach((isPressed, buttonIndex) => {
-        if (isPressed) {
-          Logger.debug(`Pressed Button Index: ${buttonIndex}`);
-          // Prevent multiple triggering
-          if (g_pressedButtonIndex == undefined) {
-            g_pressedButtonIndex = buttonIndex;
-            HandleButton(buttonIndex);
-          }
-        } else if (buttonIndex == g_pressedButtonIndex) {
-          g_pressedButtonIndex = undefined;
+    const gamepad = navigator.getGamepads()[App.state.gamepad_index];
+    if (!gamepad) {
+      App.Logger.error("setInterval: Gamepad not found");
+      return;
+    }
+
+    for (let index = 0; index < gamepad.buttons.length; index++) {
+      const isPressed = gamepad.buttons[index].pressed;
+      if (isPressed) {
+        if (App.state.pressed_button_index === -1) {
+          App.Logger.debug(`Pressed Button Index: ${index}`);
+          App.state.pressed_button_index = index;
+          HandleButton(index);
         }
-      });
+      } else if (index === App.state.pressed_button_index) {
+        App.state.pressed_button_index = -1;
+      }
+    }
   }, 50);
 
   ////////////////////
@@ -326,20 +334,26 @@ const Button_t = {
   ////////////////////
 
   window.addEventListener("gamepadconnected", (event) => {
-    Logger.info(
+    if (!App.Actions.ValidateURL()) return;
+
+    App.Logger.info(
       `Gamepad Connected; \nIndex: ${event.gamepad.index}; \nName: ${event.gamepad.id}`,
     );
-    g_gamepadIndex = event.gamepad.index;
+
+    App.state.gamepad_index = event.gamepad.index;
+    App.Actions.InitApp();
   });
 
   window.addEventListener("gamepaddisconnected", (event) => {
-    if (event.gamepad.index == g_gamepadIndex) {
-      Logger.info(
+    if (!App.Actions.ValidateURL()) return;
+
+    if (event.gamepad.index == App.state.gamepad_index) {
+      App.Logger.info(
         `Gamepad Disconnected; \nIndex: ${event.gamepad.index}; \nName: ${event.gamepad.id}`,
       );
-      g_gamepadIndex = undefined;
+
+      App.state.gamepad_index = -1;
+      if (App.state.observer) App.state.observer.disconnect();
     }
   });
-
-  InitObserver();
 })();
